e16f3ba8a7e07a90231a065e335f061cceac0b00
Niclas Roßberger <niclas@nidomiro.de>
2023-05-15 07:07:38 +0200

    feat(nest-es): starts up with esm

--- apps/nest-esm/webpack.config.cjs
+++ apps/nest-esm/webpack.config.cjs
@@ -1,7 +1,9 @@
 const { composePlugins, withNx } = require('@nrwl/webpack');
+const util = require('util');
+const nodeExternals = require('webpack-node-externals');
 
 // Nx plugins for webpack.
-module.exports = composePlugins(withNx(), (config) => {
+const configFunc = composePlugins(withNx(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
   config.resolve.extensionAlias = {
@@ -11,3 +13,40 @@ module.exports = composePlugins(withNx(), (config) => {
   };
   return config;
 });
+
+const configFuncWithOverrides = (config, ctx) => {
+  return configFunc(config, ctx).then((config) => {
+    /*
+    Changes here copied from https://github.com/nrwl/nx/issues/7872#issuecomment-997460397
+    */
+    config.externalsPresets = {
+      node: true,
+    };
+
+    config.output = {
+      ...config.output,
+      module: true,
+      libraryTarget: 'module',
+      chunkFormat: 'module',
+      library: {
+        type: 'module'
+      },
+      environment: {
+        module: true
+      }
+    }
+
+    config.experiments = {
+      outputModule: true
+    }
+
+    config.externals = nodeExternals({
+      importType: 'module'
+    })
+
+  return config
+
+  });
+}
+
+module.exports = configFuncWithOverrides;
